124accc453328d6a0938cb4ea4fdbe3e1e654585
Mushkhushu <flavie.houlette@laposte.net>
2026-04-14 11:48:23 +0200

    refacto vars

--- TP2/variables.tf
+++ TP2/variables.tf
@@ -1,52 +1,56 @@
 #variables.tf : toutes les déclarations de variables
 variable "student_name" {
-  description = "Prénom de l'étudiant"
-  type        = string
-  default     = "flavie"
+    description = "Prénom de l'étudiant"
+    type = string
+    default = "flavie"
 }
 variable "promo_name" {
-  description = "Promo de l'étudiant"
-  type        = string
-  default     = "READL006"
+    description = "Promo de l'étudiant"
+    type = string
+    default = "READL006"
 }
 variable "environment" {
-  description = "environment de déploiement"
-  type        = string
-  default     = "dev"
-  validation {
-    condition     = contains(["dev", "test", "prod"], var.environment)
-    error_message = "Valeurs : dev, test, prod."
-  }
+    description = "environment de déploiement"
+    type = string
+    default = "dev"
+    validation {
+        condition = contains(["dev","test","prod"], var.environment)
+        error_message = "Valeurs : dev, test, prod."
+    }
 }
 variable "region" {
-  description = "Région"
-  type        = string
-  default     = "us-east-1"
-  validation {
-    condition     = var.region == "us-east-1"
-    error_message = "Seule us-east-1 est autorisée"
-  }
+    description = "Région"
+    type = string
+    default = "us-east-1"
+        validation {
+        condition = var.region == "us-east-1"
+        error_message = "Seule us-east-1 est autorisée"
+    }
 }
 variable "vpc_cidr" {
-  description = "CIDR"
-  type        = string
-  default     = "10.10.0.0/16"
+    description = "CIDR"
+    type = string
+    default = "10.10.0.0/16"
 }
 variable "subnets" {
-  type = map(object({
-    cidr_block = string
-    az         = string
-    public     = bool
-  }))
-  default = {
-    public = { cidr_block = "10.10.1.0/24"
-    az = "us-east-1a", public = true }
-    private = { cidr_block = "10.10.2.0/24"
-    az = "us-east-1b", public = false }
-  }
+    type = map(object({
+        cidr_block = string
+        az = string
+        public = bool
+    }))
+    default = {
+        public = { 
+            cidr_block = "10.10.1.0/24"
+            az = "us-east-1a", 
+            public = true }
+        private = { 
+            cidr_block = "10.10.2.0/24"
+            az = "us-east-1b", 
+            public = false }
+    }
 }
 variable "key_pair_name" {
-  description = "ssh key pairing for nginx"
-  type        = string
-  default     = "tf-flavie-dev-key"
+    description = "ssh key pairing for nginx"
+    type = string
+    default = "tf-flavie-dev-key"
 }
